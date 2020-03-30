f30fe7ee0b48c785d33a6046e5647f7d55a15099
Allyson Reyes <areyes986@gmail.com>
2020-03-29 19:36:32 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# 401-DataStructures-Algorithm
+# data-structures-and-algorithms
+
+- [Array Shift](./code-challenges/ArrayShift/arrayshift.md)  
+- [Binary Search](./code-challenges/arrayBinarySeach/BinarySearch/BinarySearch/binarysearch.md)
+- [Linked List](./code-challenges/LinkedList/linkedlist.md)
+- [Insert Before](./code-challenges/LinkedList/insertBefore.md)
+- [Kth At The End](./code-challenges/LinkedList/Kth.md)
+- [Merge Linked Lists](./code-challenges/LLMerge/LLMerge.md)
---
+++ challenges/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LinkedList.Class;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            LinkList link = new LinkList();
+            link.Insert(5);
+            link.Insert(12);
+            link.Insert(43);
+            link.Insert(3);
+
+            bool result = link.Includes(5);
+            Console.WriteLine(result);
+
+            Console.WriteLine(link.ToString());
+
+            Console.WriteLine(link.KthFromEnd(2));
+        }
+
+    }
+}
