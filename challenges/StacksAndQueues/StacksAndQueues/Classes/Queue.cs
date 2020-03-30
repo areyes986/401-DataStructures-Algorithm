77b3d58d1eb752da178bffbf51458044c13c5cff
Allyson Reyes <areyes986@gmail.com>
2020-03-29 21:24:47 -0700

    stack methods and tests done

---
+++ challenges/StacksAndQueues/StacksAndQueues/Classes/Queue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    public class Queue
+    {
+        public Node Front { get; set; }
+
+        public void Enqueue(int number)
+        {
+            Queue rear = new Queue();
+
+
+
+        }
+    }
+}
