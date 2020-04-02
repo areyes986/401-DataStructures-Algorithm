3cf0c04e9e104749e7dfdf3dad2ab29f3a54831a
Allyson Reyes <areyes986@gmail.com>
2020-04-02 13:23:04 -0700

    not completed method for brackets

---
+++ challenges/MultiBracketValidation/MultiBracketValidation/Classes/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiBracketValidation.Classes
+{
+    class Stack
+    {
+        public Node Top { get; set; }
+
+        public void Push(string input)
+        {
+            Node node = new Node();
+            node.Value = input;
+            node.Next = Top;
+            Top = node;
+        }
+
+        public string Pop()
+        {
+            if (Top == null)
+            {
+                throw new Exception("There is nothing in the stack!");
+            }
+
+            Node temp = Top;
+            Top = Top.Next;
+            temp.Next = null;
+            return temp.Value;
+        }
+
+        public string Peek()
+        {
+            if (Top == null)
+            {
+                throw new Exception("There is nothing in the stack!");
+            }
+            return Top.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            if (Top != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
---
+++ challenges/MultiBracketValidation/bracket.md
@@ -0,0 +1,41 @@
+# Multi Bracket Validation
+
+*Author: Allyson Reyes*
+
+---
+
+### Problem Domain
+Create a method that takes a string input and return a boolean if the input has balanced brackets.
+
+---
+
+### Inputs and Expected Outputs
+
+| Input | Expected Output |
+| :----------- | :----------- |
+| `{}[]()` | TRUE |
+| `}[()` | FALSE |
+
+
+---
+### Big O
+| Time | Space |
+| :----------- | :----------- |
+| O(n) | O(n) |
+
+
+
+---
+
+### Whiteboard Visual
+![LL Merge](../../assets/bracket.png)
+
+
+---
+
+### Change Log  
+1.2: *Readme* - 04.02.2020  
+1.3: *tried out method* - 04.02.2020  
+1.2: *created the classes of node and stack* - 04.02.2020  
+1.1: *Started out the whiteboarding* - 04.02.2020
+
