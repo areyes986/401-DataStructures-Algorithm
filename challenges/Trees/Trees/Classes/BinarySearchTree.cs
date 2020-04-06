377c28b093fbd131092858ad0b88d27f132b52fd
Allyson Reyes <56554680+areyes986@users.noreply.github.com>
2020-04-05 22:47:09 -0700

    added the add and contains method in the binaryseachtree class

---
+++ challenges/Trees/Trees/Classes/BinarySearchTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.Classes
+{
+    class BinarySearchTree
+    {
+        public Node Root { get; set; }
+
+        public Node Add(Node root, int value)
+        {
+            if (root == null)
+            {
+                root.Value = value;
+            }
+            else if (value < root.Value)
+            {
+                root.LeftChild = Add(root.LeftChild, value);
+            }
+            else
+            {
+                root.RightChild = Add(root.RightChild, value);
+            }
+            Root = root;
+            return Root;
+        }
+
+        public bool Contains (int value)
+        {
+            if (value == Root.Value)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ challenges/Trees/Trees/Classes/BinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.Classes
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+        public int[] PreOrder()
+        {
+            return Root.Value;
+            if (Root.LeftChild != null && Root.RightChild != null)
+            {
+                PreOrder(Root.LeftChild);
+                PreOrder(Root.RightChild);
+            }
+           
+
+
+
+        }
+    }
+}
