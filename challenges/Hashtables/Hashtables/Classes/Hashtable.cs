5823eb6c8034a9a0db18b77134dffc93a9c49354
Allyson Reyes <56554680+areyes986@users.noreply.github.com>
2020-04-27 09:08:55 -0700

    completed methods

---
+++ challenges/Hashtables/Hashtables/Classes/Hashtable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashtables.Classes
+{
+    public class Hashtable
+    {
+
+        public string[] Bucket = new string[50];
+
+        public void Add(string key, string value)
+        {
+            int num = Hash(key);
+            Bucket[num] = value;
+        }
+
+
+        public int Hash(string key)
+        {
+            int sum = 0;
+            for (int i = 0; i < key.Length; i++)
+            {
+                sum += key[i];
+            }
+
+            if (sum > Bucket.Length)
+            {
+                Array.Resize(ref Bucket, sum + Bucket.Length);
+            }
+            return sum;
+        }
+
+        //get: takes in the key and returns the value from the table.
+        //contains: takes in the key and returns a boolean, indicating if the key exists in the table already.
+
+        public string Get(string key)
+        {
+            int num = Hash(key);
+            return Bucket[num];
+        }
+
+        public bool Contains(string key)
+        {
+            int num = Hash(key);
+            if (Bucket[num] != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ challenges/Hashtables/Hashtables/Program.cs
@@ -0,0 +1,25 @@
+﻿using Hashtables.Classes;
+using System;
+
+namespace Hashtables
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AddToBucket();
+        }
+
+        public static void AddToBucket()
+        {
+            Hashtable ht = new Hashtable();
+            int mochiNum = ht.Hash("mochi is a");
+            ht.Add("mochi is a", "fool");
+            //Console.WriteLine(ht.Bucket[mochiNum]);
+            
+            Console.WriteLine(ht.Get("mochi is a"));
+        }
+
+
+    }
+}
